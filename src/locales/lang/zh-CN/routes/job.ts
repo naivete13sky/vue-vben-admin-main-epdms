dc89178353492dbd14ad6e6e1a362c482bda038c
naivete13sky <chen320821@163.com>
2023-07-18 19:55:21 +0800

    2

---
+++ src/locales/lang/zh-CN/routes/job.ts
@@ -0,0 +1,36 @@
+export default {
+  job: {
+    job: '主料号管理',
+    basic: '基础组件cc',
+    transition: '动画组件',
+    countTo: '数字动画',
+
+    scroll: '滚动组件',
+    scrollBasic: '基础滚动',
+    scrollAction: '滚动函数',
+    virtualScroll: '虚拟滚动',
+
+    tree: 'Tree',
+    treeBasic: '基础树',
+    editTree: '可搜索/工具栏',
+    actionTree: '函数操作示例',
+
+    modal: '弹窗扩展',
+    drawer: '抽屉扩展',
+    desc: '详情组件',
+
+    verify: '验证组件',
+    verifyDrag: '拖拽校验',
+    verifyRotate: '图片还原',
+
+    qrcode: '二维码组件',
+    strength: '密码强度组件',
+    upload: '上传组件',
+
+    loading: 'Loading',
+
+    time: '相对时间',
+    cropperImage: '图片裁剪',
+    cardList: '卡片列表',
+  },
+};
