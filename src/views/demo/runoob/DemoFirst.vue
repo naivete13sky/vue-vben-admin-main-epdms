61050a0d886c61dd755111a16aefaa7ead8496e0
naivete13sky <chen320821@163.com>
2023-12-14 15:35:12 +0800

    runoob 多级菜单4

--- src/views/demo/runoob/DemoFirst.vue
+++ src/views/demo/runoob/DemoFirst.vue
@@ -1,20 +1,36 @@
 <template>
   <div id="hello-vue" class="demo">
-  {{ message }}
-</div>
+    {{ message }}
+  </div>
+
+  <div id="hello-vue2" class="demo">
+    {{ message2 }}
+  </div>
 </template>
 
-
 <script lang="ts">
-  import { defineComponent } from 'vue';
+  import { defineComponent, createApp } from 'vue';
 
   export default defineComponent({
     data() {
       return {
-        message: 'Hello Vue!!'
-      }
+        message: 'Hello Vue!!',
+        message2: 'Message from HelloVueApp'
+      };
+    },
+    mounted() {
+      // 在主组件的 mounted 生命周期钩子中创建并挂载独立的 Vue 实例
+      const helloVueAppInstance = createApp({
+        data() {
+          return {
+            message2: 'Hello Vue!!2'
+          };
+        },
+        template: '<div>{{ message2 }}</div>'
+      });
+
+      // 将实例挂载到指定的元素
+      helloVueAppInstance.mount('#hello-vue2');
     }
   });
 </script>
-
-
---
+++ src/views/demo/runoob/DemoFirst1.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="hello-vue" class="demo">
+  {{ message }}
+</div>
+</template>
+
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  export default defineComponent({
+    data() {
+      return {
+        message: 'Hello Vue!!'
+      }
+    }
+  });
+</script>
+
+
